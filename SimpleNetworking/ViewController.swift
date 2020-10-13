1258d7dc3fdd957bd2f23d28d593ca37d0ee9844
cladendas <rvdagi05@yandex.ru>
2020-10-13 18:58:58 +0300

    Initial Commit

---
+++ SimpleNetworking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleNetworking
+//
+//  Created by cladendas on 13.10.2020.
+//  Copyright © 2020 cladendas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
