36885be9d064b0abede5f660daecbe773403e2c0
cladendas <rvdagi05@yandex.ru>
2020-10-14 16:20:29 +0300

    Работа с POST запросами

--- SimpleNetworking/ViewController.swift
+++ SimpleNetworking/ViewController.swift
@@ -10,6 +10,8 @@ import Foundation
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //считываем данные с сервера
     @IBAction func getRequest(_ sender: UIButton) {
         
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
@@ -18,10 +20,14 @@ class ViewController: UIViewController {
         
         //response - ответ сервера
         session.dataTask(with: url) { (data, response, error) in
-            guard let data = data,
+            guard
+                let data = data,
                 let response = response
-                else { return }
+            else { return }
             
+            print(response)
+            
+            //обработка ответа от сервера
             do {
                 let json = try JSONSerialization.jsonObject(with: data, options: [])
                 print(json)
@@ -30,7 +36,46 @@ class ViewController: UIViewController {
             }
         }.resume()
     }
+    
+    //передаём данные на сервер
     @IBAction func postRequest(_ sender: UIButton) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        //передаваемые данные на сервер
+        let userData = ["Course": "Networking,", "Lesson": "GET and POST requests"]
+        
+        var request = URLRequest(url: url)
+        //указываем тип запроса
+        request.httpMethod = "POST"
+        //правило добавления новой записи
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        //сериализуем в JSON
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData, options: []) else { return }
+        
+        //помещение данных в тело запроса
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            
+            guard
+                let data = data,
+                let response = response
+            else { return }
+            
+            print(response)
+
+            //обработка ответа от сервера
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
+        
     }
     
 }
