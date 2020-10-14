9a1299e582b753f01e62de23248400b40a445b9d
cladendas <rvdagi05@yandex.ru>
2020-10-14 12:19:19 +0300

    Получение данных с сервера и их сериализация

--- SimpleNetworking/ViewController.swift
+++ SimpleNetworking/ViewController.swift
@@ -10,5 +10,27 @@ import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func getRequest(_ sender: UIButton) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let session = URLSession.shared
+        
+        //response - ответ сервера
+        session.dataTask(with: url) { (data, response, error) in
+            guard let data = data,
+                let response = response
+                else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    @IBAction func postRequest(_ sender: UIButton) {
+    }
+    
 }
