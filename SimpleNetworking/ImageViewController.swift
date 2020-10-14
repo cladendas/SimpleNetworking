7adb8e5705eb67416d5532464ea01907515249c2
cladendas <rvdagi05@yandex.ru>
2020-10-14 09:13:59 +0300

    Добавление NavigationController

---
+++ SimpleNetworking/ImageViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SimpleNetworking
+//
+//  Created by cladendas on 13.10.2020.
+//  Copyright © 2020 cladendas. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+        
+    func fetchImage() {
+        label.isHidden = true
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
+        
+        //создание общей сессии
+        let session = URLSession.shared
+        
+        //извлечение данных по указанному url
+        //происходит ассинхронно в фоновом потоке
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.image.image = image
+                }
+            }
+        }.resume()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator.isHidden = true
+        //скроет индикатор, когда он остановится
+        activityIndicator.hidesWhenStopped = true
+        fetchImage()
+    }
+}
+
--- SimpleNetworking/ViewController.swift
+++ SimpleNetworking/ViewController.swift
@@ -1,48 +1,14 @@
 //
-//  ViewController.swift
+//  ImageViewController.swift
 //  SimpleNetworking
 //
-//  Created by cladendas on 13.10.2020.
+//  Created by cladendas on 14.10.2020.
 //  Copyright © 2020 cladendas. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var label: UILabel!
-    @IBOutlet var image: UIImageView!
-    @IBOutlet var button: UIButton!
-    @IBOutlet var activityIndicator: UIActivityIndicatorView!
-    
-    @IBAction func buttonAction(_ sender: UIButton) {
-        
-        button.isHidden = true
-        label.isHidden = true
-        activityIndicator.isHidden = false
-        activityIndicator.startAnimating()
-        
-        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
-        
-        //создание общей сессии
-        let session = URLSession.shared
-        
-        //извлечение данных по указанному url
-        //происходит ассинхронно в фоновом потоке
-        session.dataTask(with: url) { (data, response, error) in
-            if let data = data, let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self.activityIndicator.stopAnimating()
-                    self.image.image = image
-                }
-            }
-        }.resume()
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        activityIndicator.isHidden = true
-        //скроет индикатор, когда он остановится
-        activityIndicator.hidesWhenStopped = true
-    }
 }
-
